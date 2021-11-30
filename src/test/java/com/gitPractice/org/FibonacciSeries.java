d2eb2ab54efc8e28bbad05da0e938fc4e2ce64e0
hrusikesh <h.k.bisoyi128@gmail.com>
2021-11-30 19:09:46 +0530

    Fibonacci Series

--- src/test/java/com/gitPractice/org/FibonacciSeries.java
+++ src/test/java/com/gitPractice/org/FibonacciSeries.java
@@ -4,6 +4,7 @@ public class FibonacciSeries {
 
 	public static void main(String[] args) {
 		int a=0,b=1,sum=0;
+		System.out.print(a+","+b+",");
 		for(int i=2;i<10;i++) {
 			sum=a+b;
 			System.out.print(sum+",");
