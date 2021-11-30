074f14d4c693c32cb01a325c0caf47f1f998d4e7
hrusikesh <h.k.bisoyi128@gmail.com>
2021-11-30 20:30:19 +0530

    Factorial Number

---
+++ src/test/java/com/gitPractice/org/FactorialNumber.java
@@ -0,0 +1,18 @@
+package com.gitPractice.org;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int number=sc.nextInt();
+		int factorial=1;
+		for(int i=1;i<=number;i++) {
+			factorial=factorial*i;
+		}
+		System.out.println("Factorial of "+number+" is "+factorial);
+	}
+
+}
