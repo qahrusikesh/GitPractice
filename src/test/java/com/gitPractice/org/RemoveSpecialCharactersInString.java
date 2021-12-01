43a418263c9160aa5b57dc9a18c22c4a24dc61ee
hrusikesh <h.k.bisoyi128@gmail.com>
2021-12-01 17:47:58 +0530

    Searching an Element in Array

---
+++ src/test/java/com/gitPractice/org/RemoveSpecialCharactersInString.java
@@ -0,0 +1,13 @@
+package com.gitPractice.org;
+
+public class RemoveSpecialCharactersInString {
+
+	public static void main(String[] args) {
+		String s="*h_+r_+u#s@i# k><e:s}h{}@1#2$";
+		String a=s.replaceAll("[^a-zA-Z0-9]", "");
+		System.out.println(s);
+		System.out.println(a);
+
+	}
+
+}
