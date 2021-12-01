9ec4a7d1c8c5f2a56398c2261375d8a82e8393c9
hrusikesh <h.k.bisoyi128@gmail.com>
2021-12-02 00:41:01 +0530

    updated code 02122021

--- src/test/java/com/gitPractice/org/SumOfArray.java
+++ src/test/java/com/gitPractice/org/SumOfArray.java
@@ -3,7 +3,7 @@ package com.gitPractice.org;
 public class SumOfArray {
 
 	public static void main(String[] args) {
-		int a[]= {10,20,30,40,50,-20};
+		int a[]= {10,20,30,40,50};
 		int sum=0;
 		for(int i=0;i<=a.length-1;i++) {
 			sum=sum+a[i];
